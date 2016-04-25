7957240f56e7b0d953b23df11caaaa696228f9e1
Lyph <LyphCo@gmail.com>
2016-04-24 17:39:48 -0700

    3rd Optimization:Reduced to one function. O(nlogn)

--- 84) Largest Rectangle in Histogram/84) Largest Rectangle in Histogram/Source.cpp
+++ 84) Largest Rectangle in Histogram/84) Largest Rectangle in Histogram/Source.cpp
@@ -9,26 +9,64 @@
 #include <algorithm> 
 //#include "stdafx.h" //for Visual Studios 
 
-using namespace std; 
+using namespace std;
 
+int largestRectangleArea(vector<int>& heights) {
 
-
-int RectangleArea(vector<int>& heights) {
-
+	if (heights.size() == 0) { return 0; } //empty set
 	if (heights.size() == 1) { return heights[0]; } //set of 1
+	if (*max_element(heights.begin(), heights.end()) == 0) { return 0; }//multiple zeros
+	bool sorted = true;
+	int sortedMax = 0;
+	int sortedTemp = 0;
 
-	int counterTemp = 0; 
+	for (int i = 1; i < heights.size(); i++) {
+		if (heights[i] < heights[i - 1]) {
+			sorted = false;
+			break;
+		}
+	}
+
+	if (sorted)
+	{
+		for (int i = 0; i < heights.size(); i++) {
+			
+			sortedTemp = heights[i] * (heights.size() - i);
+			if (sortedTemp > sortedMax) { sortedMax = sortedTemp; }
+		}
+		return sortedMax;
+	}
+
+	int counterTemp = 0;
 	int counter = 0;
-	int tempMaxArea = 0; 
+	int tempMaxArea = 0;
 	int maxArea = 0;
 
-	heights.push_back(0);
 	int i = heights[0];
 	int k = 0;
+	bool uni;
 
-	while(i != 0) {
+	vector<int> unique{heights[0]};
+	for (int i = 0; i < heights.size(); i++) {
+		for (int j = 0; j < unique.size(); j++) {
+			if (heights[i] == unique[j])
+			{
+				uni = false;
+				break;
+			}
+			else 
+			{
+				uni = true;
+			}
+		}
+		if (uni == true) { unique.push_back(heights[i]); }
+	}
+
+	heights.push_back(0);
+
+	for (int i = 0; i < unique.size(); i++) {
 		for (int j = 0; j < heights.size(); j++) {
-			if (i <= heights[j]) {
+			if (unique[i] <= heights[j]) {
 				counterTemp++;
 			}
 			else {
@@ -38,55 +76,20 @@ int RectangleArea(vector<int>& heights) {
 				counterTemp = 0;
 			}
 		}
-		tempMaxArea = i * counter;
+		tempMaxArea = unique[i] * counter;
 		if (tempMaxArea > maxArea) { maxArea = tempMaxArea; }
 		counter = 0;
-		k++;
-		while (i == heights[k]) { k++; }
-		i = heights[k];
-		
+		counterTemp = 0;
+
 	}
 	return maxArea;
-}
-
-int largestRectangleArea(vector<int>& whole) {
-	vector<vector<int>> A;
-	int start = 0;
-	int end = 0;
-
-	if (whole.size() == 0) { return 0; } //empty set
-	if (whole.size() == 1) { return whole[0]; } //set of 1
-	if (*max_element(whole.begin(), whole.end()) == 0) { return 0; }//multiple zeros
-	//if (whole.size() == 20000) { return 100000000; }
-
-	whole.push_back(0);
-	for (int i = 0; i < whole.size();i++)
-	{
-		if (whole[i] != 0){end++;}
-
-		else {
-			vector<int> temp(whole.begin() + start, whole.begin() + end);
-			if (temp.size() != 0) {
-				A.push_back(temp);
-			}
-			end = i + 1;
-			start = i + 1;
-		}
-	}
-	int maxTemp = 0;
-	int max2 = 0;
-	for (int i = 0; i < A.size();i++) {
-		maxTemp = RectangleArea(A[i]);
-		if (maxTemp > max2) { max2 = maxTemp; }
-	}
-	return max2;
 }
 
 
 int main()
 {
-	vector<int> a{5,5,1,7,1,1,5,2,7,6};
-	
+	vector<int> a{ 0,1,0,9 };
+
 	int max = largestRectangleArea(a);
 	cout << max;
 
